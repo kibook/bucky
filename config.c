e2ffd60cccec6bf30f0746ef3c255be60a5ea3c3
kibook <annadaniau@gmail.com>
2016-12-09 16:07:36 -0500

    add generic paths for all Gopher item icons

--- config.c
+++ config.c
@@ -34,18 +34,22 @@ const char *mime_type_from_ext(char *ext)
 const char *gopher_item_icon(char type)
 {
 	switch (type) {
-		case GOPHER_ITEM_PLAIN_TEXT: return "/gophericon/icn0.png";
-		case GOPHER_ITEM_DIRECTORY: return "/gophericon/icn1.png";
-		case GOPHER_ITEM_ERROR: return "/gophericon/icn3.png";
-		case GOPHER_ITEM_BINHEX: return "/gophericon/icn4.png";
-		case GOPHER_ITEM_DOS: return "/gophericon/icn5.png";
-		case GOPHER_ITEM_SEARCH: return "/gophericon/icn7.png";
-		case GOPHER_ITEM_TELNET: return "/gophericon/icn8.png";
-		case GOPHER_ITEM_BINARY: return "/gophericon/icn9.png";
-		case GOPHER_ITEM_JPEG: return "/gophericon/icnI.png";
-		case GOPHER_ITEM_WAV: return "/gophericon/icns.png";
-		case GOPHER_ITEM_PNG: return "/gophericon/icnp.png";
-		case GOPHER_ITEM_HTML: return "/gophericon/icnh.png";
+		case GOPHER_ITEM_PLAIN_TEXT: 	return "/gophericon/icn0.gif";
+		case GOPHER_ITEM_DIRECTORY: 	return "/gophericon/icn1.gif";
+		case GOPHER_ITEM_CSO:		return "/gophericon/icn2.gif";
+		case GOPHER_ITEM_ERROR: 	return "/gophericon/icn3.gif";
+		case GOPHER_ITEM_BINHEX: 	return "/gophericon/icn4.gif";
+		case GOPHER_ITEM_DOS: 		return "/gophericon/icn5.gif";
+		case GOPHER_ITEM_UUE:		return "/gophericon/icn6.gif";
+		case GOPHER_ITEM_SEARCH: 	return "/gophericon/icn7.gif";
+		case GOPHER_ITEM_TELNET: 	return "/gophericon/icn8.gif";
+		case GOPHER_ITEM_BINARY: 	return "/gophericon/icn9.gif";
+		case GOPHER_ITEM_TN3270:	return "/gophericon/icnT.gif";
+		case GOPHER_ITEM_GIF:		return "/gophericon/icng.gif";
+		case GOPHER_ITEM_JPEG: 		return "/gophericon/icnI.gif";
+		case GOPHER_ITEM_WAV: 		return "/gophericon/icns.gif";
+		case GOPHER_ITEM_PNG: 		return "/gophericon/icnp.gif";
+		case GOPHER_ITEM_HTML: 		return "/gophericon/icnh.gif";
 	}
 
 	return "/gophericon/icn.png";
