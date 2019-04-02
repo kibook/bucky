7e9485d69decd612d4ba166919d6da93153b6043
kibukj <kibuzhj@gmail.com>
2019-04-02 09:25:43 -0400

    Add bucky.h

---
+++ bucky.h
@@ -0,0 +1,9 @@
+#ifndef BUCKY_H
+#define BUCKY_H
+
+void hexstrcpy(char *hs, char *str);
+void htmlstrncpy(char *html, char *str, size_t num);
+void urlstrncpy(char *url, char *str, size_t num);
+void unurlstrncpy(char *str, char *url, size_t num);
+
+#endif
